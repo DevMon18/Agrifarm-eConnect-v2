7522e20a6802e21fab06fba3cb578f13ad757244
Ritzmond Manzo <ritzmond.manzo@gmail.com>
2024-01-18 22:41:45 +0800

    Hide Privacy for mail.php

--- mail.php
+++ mail.php
@@ -13,8 +13,8 @@ function sendemail_verify($name, $email, $verify_token){
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                       //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'ritzmond.manzo@evsu.edu.ph';                   //SMTP username
-    $mail->Password   = '091897Ritzmondmanzo@';                  //SMTP password
+    $mail->Username   = 'Enter you gmail address';                   //SMTP username
+    $mail->Password   = 'Enter your gmail password';                  //SMTP password **Note a gmail account that has LESS SECURE APPS MUST BE NEEDED
     $mail->SMTPSecure = "tls";                                  //Enable implicit TLS encryption
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
